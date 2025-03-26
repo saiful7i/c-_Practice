6b0ab8141480788fb3ee9557ac461310d6100558
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-03-26 11:34:58 +0600

    Update MethodOverloading.cs

--- MethodOverloading.cs
+++ MethodOverloading.cs
@@ -4,18 +4,18 @@ namespace Application
 {
   class Program
   {
-    static int PlusMethodInt(int x, int y)
+    static int PlusMethod(int x, int y)
     {
       return x +y;
     }
-    static double PlusMethodDouble(double x, double y)
+    static double PlusMethod(double x, double y)
     {
       return x+y;
     }
     static void Main(string[] args)
     {
-      int myNum1 = PlusMethodInt(10,13);
-      double myNum2 = PlusMethodDouble(4.3,4.34);
+      int myNum1 = PlusMethod(10,13);
+      double myNum2 = PlusMethod(4.3,4.34);
       Console.WriteLine("Int: "+ myNum1);
       Console.WriteLine("Double: "+ myNum2);
     }
