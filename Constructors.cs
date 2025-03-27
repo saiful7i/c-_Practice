08d219a84ddb054a7ee31efe9699130673da2b50
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-03-27 11:06:52 +0600

    Create Constructors.cs

---
+++ Constructors.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Program
+//class for cha 
+{
+  class Car
+  {
+    public string model;  //field of class
+  
+    public Car() //Car class Constructor
+    {
+      model = "Mustang"; //initial value of model
+    }
+    
+    static void Main(string[] args)
+    {
+      Car Ford = new Car(); //object of the Car class
+      Console.WriteLine(Ford.model);
+    }
+  }
+}
